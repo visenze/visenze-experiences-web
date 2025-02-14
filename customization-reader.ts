cbf663290a6b607b66e1ef34a6a97dd7e8fcfd48
Wilson Kurniawan <wilson@visenze.com>
2025-02-14 19:42:23 +0800

    Add scripts to read default config values

---
+++ customization-reader.ts
@@ -0,0 +1,43 @@
+// Returns a JSON containing the default config of a specific widget type.
+// Usage: npx ts-node -P tsconfig-script.json customization-reader <CONTEXT_ID>
+
+import type { WidgetConfig } from './src/common/wigmix-core';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_7 } from './src/official-widgets/camera-search/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_8 } from './src/official-widgets/similar-search/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_9 } from './src/official-widgets/search-results-page/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_10 } from './src/official-widgets/shopping-assistant/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_11 } from './src/official-widgets/recommend-me/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_12 } from './src/official-widgets/more-like-this/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_13 } from './src/official-widgets/shop-the-look/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_14 } from './src/official-widgets/embedded-grid/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_15 } from './src/official-widgets/shoppable-lookbook/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_16 } from './src/official-widgets/shoppable-gallery/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_17 } from './src/official-widgets/icon-triggered-grid/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_18 } from './src/official-widgets/search-bar/default-config';
+import { DEFAULT_CUSTOMIZATIONS as DEFAULT_CUSTOMIZATIONS_19 } from './src/official-widgets/embedded-search-results/default-config';
+
+const configs: Record<string, WidgetConfig['customizations']> = {
+  7: DEFAULT_CUSTOMIZATIONS_7,
+  8: DEFAULT_CUSTOMIZATIONS_8,
+  9: DEFAULT_CUSTOMIZATIONS_9,
+  10: DEFAULT_CUSTOMIZATIONS_10,
+  11: DEFAULT_CUSTOMIZATIONS_11,
+  12: DEFAULT_CUSTOMIZATIONS_12,
+  13: DEFAULT_CUSTOMIZATIONS_13,
+  14: DEFAULT_CUSTOMIZATIONS_14,
+  15: DEFAULT_CUSTOMIZATIONS_15,
+  16: DEFAULT_CUSTOMIZATIONS_16,
+  17: DEFAULT_CUSTOMIZATIONS_17,
+  18: DEFAULT_CUSTOMIZATIONS_18,
+  19: DEFAULT_CUSTOMIZATIONS_19,
+};
+
+const args = process.argv.slice(2);
+
+if (args.length) {
+  console.log(
+      args.includes('--pretty-print')
+      ? JSON.stringify(configs[args[0]], null, 2)
+      : JSON.stringify(configs[args[0]]),
+  );
+}
---
+++ text-reader.ts
@@ -0,0 +1,43 @@
+// Returns a JSON containing the default texts of a specific widget type.
+// Usage: npx ts-node -P tsconfig-script.json text-reader <CONTEXT_ID>
+
+import type { LanguagePack } from './src/common/locales/locale';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_7 } from './src/official-widgets/camera-search/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_8 } from './src/official-widgets/similar-search/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_9 } from './src/official-widgets/search-results-page/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_10 } from './src/official-widgets/shopping-assistant/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_11 } from './src/official-widgets/recommend-me/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_12 } from './src/official-widgets/more-like-this/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_13 } from './src/official-widgets/shop-the-look/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_14 } from './src/official-widgets/embedded-grid/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_15 } from './src/official-widgets/shoppable-lookbook/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_16 } from './src/official-widgets/shoppable-gallery/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_17 } from './src/official-widgets/icon-triggered-grid/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_18 } from './src/official-widgets/search-bar/default-config';
+import { DEFAULT_TEXTS as DEFAULT_TEXTS_19 } from './src/official-widgets/embedded-search-results/default-config';
+
+const configs: Record<string, LanguagePack> = {
+  7: DEFAULT_TEXTS_7,
+  8: DEFAULT_TEXTS_8,
+  9: DEFAULT_TEXTS_9,
+  10: DEFAULT_TEXTS_10,
+  11: DEFAULT_TEXTS_11,
+  12: DEFAULT_TEXTS_12,
+  13: DEFAULT_TEXTS_13,
+  14: DEFAULT_TEXTS_14,
+  15: DEFAULT_TEXTS_15,
+  16: DEFAULT_TEXTS_16,
+  17: DEFAULT_TEXTS_17,
+  18: DEFAULT_TEXTS_18,
+  19: DEFAULT_TEXTS_19,
+};
+
+const args = process.argv.slice(2);
+
+if (args.length) {
+  console.log(
+      args.includes('--pretty-print')
+      ? JSON.stringify(configs[args[0]], null, 2)
+      : JSON.stringify(configs[args[0]]),
+  );
+}
