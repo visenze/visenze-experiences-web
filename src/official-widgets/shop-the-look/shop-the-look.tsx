115eb98570bcbd01e4e07d50a65a8c803801cc29
Nicholas Goh <nicholas.goh@visenze.com>
2025-03-21 12:43:04 +0800

    ui fixed contd

--- src/official-widgets/shop-the-look/shop-the-look.tsx
+++ src/official-widgets/shop-the-look/shop-the-look.tsx
@@ -177,7 +177,7 @@ const ShopTheLook: FC<ShopTheLookProps> = ({ productId }) => {
 
               <div className='items-center justify-center text-primary md:flex md:flex-row md:gap-4 lg:gap-0'>
                 {/* Reference Image */}
-                <div className='px-1 md:w-3/10 lg:w-1/4'>
+                <div className='px-1 md:w-3/10 xl:w-1/4'>
                   <div className='relative'>
                     {objectDots.map((obj, index) => (
                         <button
