422b1a008cf6924ff03a34b5f004663590308f9a
Wilson Kurniawan <wilson@visenze.com>
2025-03-25 11:34:14 +0800

    Move camera-search history check to the same test case as find similar

--- src/official-widgets/embedded-search-results/embedded-search-results.spec.tsx
+++ src/official-widgets/embedded-search-results/embedded-search-results.spec.tsx
@@ -511,12 +511,22 @@ describe('embedded-search-result', () => {
       const inactiveHistory = testComponent.getByTestId('wigmix-inactive-product');
       inactiveHistory.click();
     });
+    expect(counter).toEqual(3);
+
+    // Active history should be replaced
     const activeHistoryImage = testComponent.getByTestId('wigmix-active-product-history-image');
     expect(activeHistoryImage.getAttribute('src')).toEqual('test-im-url');
+
+    // The previously used image should be moved to inactive history
     const inactiveHistoryImages = testComponent.queryAllByTestId('wigmix-inactive-product-history-image');
     expect(inactiveHistoryImages.length).toEqual(1);
     expect(inactiveHistoryImages[0].getAttribute('src')).toEqual('https://main-image-5');
-    expect(counter).toEqual(3);
+
+    // Verify that the results go back to the unscrambled order
+    const productCardImages = testComponent.queryAllByTestId('wigmix-product-card-image');
+    productCardImages.forEach((productCardImage, idx) => {
+      expect(productCardImage.getAttribute('src')).toEqual(`https://main-image-${idx + 1}`);
+    });
   });
 
   it('should apply filter successfully in desktop view', () => {
