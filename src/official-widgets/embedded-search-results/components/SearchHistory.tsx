e60f5da1394de2185709e44e92d9b58260b87f4f
Nicholas Goh <nicholas.goh@visenze.com>
2025-04-03 18:03:12 +0800

    revert conditional statement

--- src/official-widgets/embedded-search-results/components/SearchHistory.tsx
+++ src/official-widgets/embedded-search-results/components/SearchHistory.tsx
@@ -60,8 +60,7 @@ const SearchHistory = ({
                   entry.id === getActiveHistoryId() ? 'border border-gray-500' : 'opacity-60',
                 )}
                 onClick={() => {
-                  if (entry.id !== getActiveHistoryId()
-                    || ((entry.box && activeHistory?.box) && entry.box !== activeHistory.box)) {
+                  if (entry.id !== getActiveHistoryId()) {
                     onHistorySelect(entry);
                   }
                 }}
