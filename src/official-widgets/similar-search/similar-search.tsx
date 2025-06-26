a7a4c96746c69b304745f738dc25313d659a998d
Nicholas Goh <nicholas.goh@visenze.com>
2025-06-26 14:24:53 +0800

    added pid search for similar-search

--- src/official-widgets/similar-search/similar-search.tsx
+++ src/official-widgets/similar-search/similar-search.tsx
@@ -22,11 +22,12 @@ enum ScreenType {
 }
 
 interface SimilarSearchProps {
+  pid: string;
   imUrl: string;
   renderModalWithoutPortal?: boolean;
 }
 
-const SimilarSearch: FC<SimilarSearchProps> = ({ imUrl, renderModalWithoutPortal }) => {
+const SimilarSearch: FC<SimilarSearchProps> = ({ pid, imUrl, renderModalWithoutPortal }) => {
   const { widgetConfig, widgetClient, darkMode } = useContext(WidgetDataContext);
   const { appSettings, customizations } = widgetConfig;
   const breakpoint = useBreakpoint();
@@ -191,7 +192,7 @@ const SimilarSearch: FC<SimilarSearchProps> = ({ imUrl, renderModalWithoutPortal
 
   useEffect(() => {
     if (!productResults.length && dialogVisible) {
-      setImage({ imgUrl: imUrl });
+      setImage(pid ? { pid } : { imgUrl: imUrl });
     }
   }, [dialogVisible]);
 
