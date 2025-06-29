f8542db0e5b8e34a31089e5676f8643c76d1accd
Erik Rutledge <erik.rutledge@visenze.com>
2025-06-29 08:53:57 -0400

    feat: update app.css styles and add productCard border to default config

--- src/official-widgets/in-page-carousel/default-config.ts
+++ src/official-widgets/in-page-carousel/default-config.ts
@@ -83,6 +83,10 @@ export const DEFAULT_CUSTOMIZATIONS: WidgetConfig['customizations'] = {
   productCard: {
     imageAspectRatio: '2 / 3',
     openLinksInNewTab: false,
+    border: {
+      width: 1,
+      color: '#E5E7EB',
+    },
     price: {
       show: true,
       font: {
